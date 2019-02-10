15e9d8c64ae5da806875747ded2bb5ad9f925e44
Frank Tegtmeyer <frank@tegtmeyer.net>
2019-02-10 11:33:19 +0100

    CAPABILITIES_NAMED_IAM required

--- aws/deploy.sh
+++ aws/deploy.sh
@@ -4,7 +4,7 @@
 
 # Effectively this runs the following command:
 # aws-vault exec forumdev -- aws cloudformation deploy --stack-name photo-forum
-#      --template-file cloudformation.yml --capabilities CAPABILITY_IAM`
+#      --template-file cloudformation.yml --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM`
 
 # compile the deployment script
 tsc
