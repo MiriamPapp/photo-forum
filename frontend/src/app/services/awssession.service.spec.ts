ba7c2818b6a8e963f7c52fe719f4078f625621f5
Frank Tegtmeyer <frank@tegtmeyer.net>
2019-02-27 22:43:01 +0100

    Add test for awssessionservice

--- frontend/src/app/services/awssession.service.spec.ts
+++ frontend/src/app/services/awssession.service.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 
 import { AwssessionService } from './awssession.service';
+import { throwError } from 'rxjs';
 
 describe('AwssessionService', () => {
   beforeEach(() => TestBed.configureTestingModule({}));
@@ -9,4 +10,13 @@ describe('AwssessionService', () => {
     const service: AwssessionService = TestBed.get(AwssessionService);
     expect(service).toBeTruthy();
   });
+
+  it('should return false in isLoggedin when S3 listing fails', () => {
+    const s3Spy = jasmine.createSpyObj('S3', ['listObjectsV2']);
+    s3Spy.listObjectsV2.and.returnValue(throwError('error'));
+    
+    const awssessionservice = new AwssessionService();
+    expect(awssessionservice.isLoggedIn()).toBe(false);
+  });
+
 });
