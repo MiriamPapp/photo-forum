cee3e1033a279eacc58c8122646fa400ed240ee5
Frank Tegtmeyer <frank@tegtmeyer.net>
2020-02-26 20:54:43 +0100

    abandon repository

--- frontend/src/environments/environment.prod.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: true,
-  forumversion: '0.1',
-  region: 'eu-central-1',
-  contentBucketName: 'photo-forum-contentsbucket-1bmeh3caiq82w',
-  contentBucketEndpoint: 's3.dualstack.eu-central-1.amazonaws.com'
-};
